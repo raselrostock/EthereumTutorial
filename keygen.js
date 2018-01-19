0fb71a4011c19bf6d303ec9dcfa33bb87f15ea0b
raselrostock <raselrostock@gmail.com>
2018-01-19 09:51:42 +0200

    Initial upload to keypair'

---
+++ keygen.js
@@ -0,0 +1,14 @@
+var EthUtil= require("ethereumjs-util");
+const args = process.argv[2];
+var hexToBytes = function(hex)
+{
+	var bytes=[];
+	for(  c =0; c< hex.length; c+=2)
+		bytes.push(parseInt(hex.substr(c,2),16));
+	return bytes;
+}
+var privateKeyToAddress = function(privateKey){
+	return "0x"+EthUtil.privateToAddress(hexToBytes(privateKey)).toString('hex');
+}
+var m=privateKeyToAddress(args);
+console.log(m);
